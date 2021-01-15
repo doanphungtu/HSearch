c37be24491027819107e26116c8c2ad24795b393
doanphungtu <doanphungtu@gmail.com>
2021-01-16 00:22:46 +0700

    Complete phone auth

--- android/app/src/main/java/com/newapp/MainActivity.java
+++ android/app/src/main/java/com/newapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.newapp;
+package com.tudpapp;
 
 import com.facebook.react.ReactActivity;
 
--- src/Screen/SplashScreen.js
+++ src/Screen/SplashScreen.js
@@ -1,13 +1,22 @@
-import React, {useEffect} from 'react';
-import {Image, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Button, Image,  View} from 'react-native';
 import Images from '../Theme/Images';
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 
 // Styles
 import styles from './Styles/SplashScreenStyles';
+import auth from '@react-native-firebase/auth';
 
 const SplashScreen = (props) => {
+  async function signInWithPhoneNumber(phoneNumber) {
+    try {
+      const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+      console.log(confirmation);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <View style={styles.container}>
       {/* <Image
@@ -15,6 +24,12 @@ const SplashScreen = (props) => {
         resizeMode="stretch"
         style={{width: '100%', height: '100%'}}
       /> */}
+      <View style={{marginTop: 100}}>
+        <Button
+          title="Click"
+          onPress={() => signInWithPhoneNumber('+84968312091')}
+        />
+      </View>
     </View>
   );
 };
